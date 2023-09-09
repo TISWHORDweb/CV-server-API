efc0960903659d9d8c9e45f5af1b1a0ae96737e1
Batimehin Emmanuel Oluwatimilehin <ebatimehin@gmail.com>
2023-09-09 13:17:55 +0100

    education

--- routes/route.user.js
+++ routes/route.user.js
@@ -8,6 +8,8 @@ const router = express.Router();
 const CoreError = require('./../core/core.error');
 const {  userSettings, singleUser, deleteUser,userHomeData, audit, singleAudit, deleteAudit, allUser, changePassword } = require('../controller/controller.user');
 const { editCertificates, certificate, singleCertificate, allCertificate, userCertificate, deleteCertificate } = require('../controller/controller.certificate');
+const { education, editEducation, singleEducation, allEducation, userEducation, deleteEducation } = require('../controller/controller.education');
+const { editLanguage, language, singleLanguage, allLanguage, userLanguage, deleteLanguage } = require('../controller/controller.language');
 
 /**
  * auth routes
@@ -27,8 +29,23 @@ router.post('/certificate', certificate);
 router.get('/certificate/:id', singleCertificate);
 router.get('/certificate/all', allCertificate);
 router.get('/certificate/user/:id', userCertificate);
-router.delete('/certificate/delete/:id', deleteCertificate);
+router.delete('/certificate/deletes', deleteCertificate);
 
+//Education
+router.put('/education/edit', editEducation);
+router.post('/education', education);
+router.get('/education/:id', singleEducation);
+router.get('/education/all', allEducation);
+router.get('/education/user/:id', userEducation);
+router.delete('/education/deletes', deleteEducation);
+
+//Language
+router.put('/language/edit', editLanguage);
+router.post('/language', language);
+router.get('/language/:id', singleLanguage);
+router.get('/language/all', allLanguage);
+router.get('/language/user/:id', userLanguage);
+router.delete('/language/deletes', deleteLanguage);
 
 /**
  * Export lastly
